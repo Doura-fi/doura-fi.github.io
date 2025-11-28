c63b08292280b50456fbfab5c8a38199eeacebb4
lpythu <lpythu@outlook.com>
2025-11-28 15:27:19 +0800

    Updates

--- _nuxt/VUEyBxvH.js
+++
@@ -1,1 +0,0 @@
-import s from"./BqoR7jTq.js";import{f as r,b5 as u,a4 as c,G as f}from"./Du6Um4kM.js";const m=r({name:"Markdown",extends:s,setup(t){const{parent:n}=u(),{between:e,default:a}=c(),o=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:o,between:e,parent:n}}}),i=Object.assign(m,{__name:"Markdown"});export{i as default};
